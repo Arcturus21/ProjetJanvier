993bbc3136bdbe75b6a26c0fbc2a7364fca3a130
Arcturus21 <thibault.legorju@gmail.com>
2018-01-20 10:28:21 +0100

    Initialisation fichier projet

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+void clamp(sf::CircleShape& shape, sf::RenderWindow& window);
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Janvier");
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if(event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::Escape)
+                    window.close();
+            }
+        }
+
+        shape.setPosition((sf::Vector2f)(sf::Mouse::getPosition(window))-sf::Vector2f(shape.getRadius(), shape.getRadius()));
+        clamp(shape,window);
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+
+void clamp(sf::CircleShape& shape, sf::RenderWindow& window)
+{
+    float xTemp,xDef,yTemp,yDef;
+    xTemp=shape.getPosition().x;
+    yTemp=shape.getPosition().y;
+
+    if(xTemp<0)
+        xTemp=0;
+    else if(xTemp+shape.getLocalBounds().width>window.getSize().x)
+        xTemp=window.getSize().x-shape.getLocalBounds().width;
+
+    if(yTemp<0)
+        yTemp=0;
+    else if(yTemp+shape.getLocalBounds().height>window.getSize().y)
+        yTemp=window.getSize().y-shape.getLocalBounds().height;
+
+    shape.setPosition(xTemp,yTemp);
+}
